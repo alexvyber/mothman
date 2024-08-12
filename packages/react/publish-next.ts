cecc7b1b5e6e009b558033e1ba5efc5fa10caa35
Alexey Sokolov <alexvyber@gmail.com>
2024-08-12 22:00:14 +0300

    gs

---
+++ packages/react/publish-next.ts
@@ -0,0 +1,23 @@
+import {  execSync } from "child_process"
+import { writeFile } from "fs/promises"
+import pkg from "./package.json"
+import { execAsync } from "./utils/exec-async"
+
+const version = `0.0.0-next-${execSync("git rev-parse --short HEAD").toString().trim()}`
+console.log(`publishing ${version}`)
+
+const oldVersion = pkg.version
+pkg.version = version
+
+await writeFile("./package.json", JSON.stringify(pkg, null, 2))
+
+try {
+  const stdout = await execAsync("npm publish --tag next")
+  console.log(stdout)
+} catch (e) {
+  console.log(e)
+  console.log("Publish failed, reverting package.json")
+}
+
+pkg.version = oldVersion
+await writeFile("./package.json", JSON.stringify(pkg, null, 2))
