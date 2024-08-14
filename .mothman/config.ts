31695af1376d1c739d8a85c2a72f8668014fd6d0
Alexey Sokolov <alexvyber@gmail.com>
2024-08-14 18:29:57 +0300

    redo setup

---
+++ .mothman/config.ts
@@ -0,0 +1,9 @@
+export default {
+  stories: ["lib/app/src/tmp/**/*.stories.*"],
+  appendToHead: `<style>.append {}</style>`,
+  addons: {
+    a11y: {
+      enabled: true,
+    },
+  },
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# mothman 
+
+mothman is an environment to develop, test, and share your React components, probaly using some AI agents... You know?
