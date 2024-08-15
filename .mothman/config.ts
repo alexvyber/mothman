0b3105d91dd8e8cb204166b1b72c981c5defadc4
Alexey Sokolov <alexvyber@gmail.com>
2024-08-15 23:21:31 +0300

    more reset

--- .mothman/config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  stories: ["lib/app/src/tmp/**/*.stories.*"],
-  appendToHead: `<style>.append {}</style>`,
-  addons: {
-    a11y: {
-      enabled: true,
-    },
-  },
-}
--- publish-next.ts
+++ publish-next.ts
@@ -1,24 +1,23 @@
 import { execSync } from "node:child_process"
 import { writeFile } from "node:fs/promises"
-import { execAsync } from "./lib/shared/exec-async"
 
 import pkg from "./package.json"
+import { execAsync } from "./src/shared/exec-async"
+import { getErrorMessage } from "./src/shared/get-error-message"
+import { logger } from "./src/shared/logger"
 
-const version = `0.0.0-next-${execSync("git rev-parse --short HEAD").toString().trim()}`
-console.log(`publishing ${version}`)
-
-const oldVersion = pkg.version
-pkg.version = version
+const tmpVersion = `0.0.0-next-${execSync("git rev-parse --short HEAD").toString().trim()}`
+const currentVersion = pkg.version
+pkg.version = tmpVersion
 
 await writeFile("./package.json", JSON.stringify(pkg, null, 2))
 
 try {
-  const stdout = await execAsync("npm publish --tag next")
-  console.log(stdout)
-} catch (e) {
-  console.log(e)
-  console.log("Publish failed, reverting package.json")
+  logger.info(await execAsync("npm publish --tag next"))
+} catch (error) {
+  logger.error(getErrorMessage(error))
+  logger.error("Publish failed, reverting package.json")
 }
 
-pkg.version = oldVersion
+pkg.version = currentVersion
 await writeFile("./package.json", JSON.stringify(pkg, null, 2))
